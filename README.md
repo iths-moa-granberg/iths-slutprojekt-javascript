329ff521cee8fc8b227f80d5adedb03deb481266
Moa Granberg <moa.granberg@hotmail.com>
2019-11-06 13:48:35 +0100

    readme, update generate cells

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 # Slutprojekt JavaScript
 
 ## Description
+Sudoku generater and solver
 
 ## Features
+Generate sudoku
+Solve sudoku
+
+extra:
+Difficulty levels
+
+extraextra:
+Possibility to solve manually
+Save unfinished to localstorage
 
 ## Libraries
+no
--- main.js
+++ main.js
@@ -1,32 +1,62 @@
-//Generate Sudoku
-//array med nummer alt null
-//fyll board med siffror
-//bara en av aktiv siffra inom row/column/square
-//checkfunktion eller if-sats?
-//ta bort x antal siffor svårighetsgrad
-
 class Cell {
+    constructor() {
+        this.value = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    }
+
     markup() {
         const sudokuWrapper = document.querySelector('.sudoku-wrapper');
         const newCell = document.createElement('div');
+        const newCellValue = document.createElement('p');
         newCell.classList.add('cell');
         sudokuWrapper.appendChild(newCell);
-        // this.cell = newCell;
+        newCell.appendChild(newCellValue);
+    }
+
+    setRandomValue() { //tar bort alla utom random number från this.value
+        let num = this.value.sort(function () { return .5 - Math.random() })[0];        
+        this.value = num;
     }
 }
 
 class Board {
-    generateCells() {
-        for (let i = 0; i < 81; i++) {
-            let cell = new Cell();
-            cell.markup();
+    constructor() {
+        this.cellList = [];
+    }
+
+    generateCells() { //this.cellList[radIndex][columnIndex]
+        for (let i = 0; i < 9; i++) {
+            let cells = [];
+            for (let j = 0; j < 9; j++) {
+                let cell = new Cell();
+                cell.markup();
+                cells.push(cell);
+            }
+            this.cellList.push(cells);
         }
+    }
+
+    setCellValue() {
+        for (let i = 0; i < 9; i++) { //row-counter
+            for (let j = 0; j < 9; j++) { //cell & column-counter
+                this.cellList[i][j].setRandomValue();                
+                this.clearRow(this.cellList[i], this.cellList[i][j].value, j);
+            }
+        }
+    }
+
+    clearRow(row, number, skipCellIndex){ //row = array, number = värdet som ska tas bort, skipCellIndex = i som ej ska tas bort
+        this.cellList[row] 
+        for (let i in this.cellList[row]) {
+            
+        }
+
     }
 }
 
 function init() {
     let board = new Board();
     board.generateCells();
+    board.setCellValue();
 }
 
 init();
